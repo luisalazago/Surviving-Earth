bbcc4869a670d3177247bd6c0824e8073d516db3
PriCamEsteban <esteban1251994@hotmail.com>
2022-04-28 08:59:33 -0500

    nn3

---
+++ juego-Surviving-Eater/Scripts/panelControlEdificioCentral.gd
@@ -0,0 +1,20 @@
+extends Panel
+
+
+
+func _ready():
+	
+	pass # Replace with function body.
+
+
+
+func activarPanel():
+	visible = !visible
+	
+
+func _on_Button_button_down():
+	activarPanel()
+	pass # Replace with function body.
+
+
+
